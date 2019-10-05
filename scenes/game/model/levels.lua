11d2f76e058768de5270ac72203352340740896d
d954mas <d954mas@mail.ru>
2019-10-05 15:11:41 +0300

    add start scene

--- scenes/game/model/levels.lua
+++ scenes/game/model/levels.lua
@@ -15,8 +15,7 @@ M.LEVELS = {
 function M.load_level(name)
 	local time = os.clock()
 	--local data = assert(sys.load_resource("/assets/levels/result/" .. name .. ".json","no lvl:" .. name))
-	local data = "{}"
-	local lvl = Level(json.decode(data))
+	local lvl = Level({})
 	COMMON.d("lvl:" .. name .. " loaded. Time:" .. (os.clock()-time),TAG)
 	return lvl
 end
---
+++ scenes/start/start_scene.lua
@@ -0,0 +1,33 @@
+local BaseScene = require "libs.sm.scene"
+local COMMON = require "libs.common"
+local GAME_CONTROLLER = require "scenes.game.model.game_controller"
+local SM = require "libs.sm.sm"
+
+---@class StartScene:Scene
+local Scene = BaseScene:subclass("StartScene")
+function Scene:initialize()
+    BaseScene.initialize(self, "StartScene", "/start#proxy", "start:/scene_controller")
+end
+function Scene:on_show()
+    COMMON.input_acquire()
+end
+
+function Scene:on_hide()
+    COMMON.input_release()
+end
+
+
+function Scene:on_update(dt)
+    BaseScene.on_update(self,dt)
+end
+
+function Scene:on_input(action_id, action)
+    return GAME_CONTROLLER:on_input(action_id,action)
+end
+
+function Scene:on_message(message_id, message, sender)
+    if message_id == COMMON.HASHES.MSG_POST_UPDATE then GAME_CONTROLLER:post_update(self.dt) end
+end
+
+
+return Scene
