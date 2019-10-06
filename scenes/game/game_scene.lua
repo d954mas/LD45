c985855675e81a05ea5a06a3fcc57f0261f228cf
d954mas <d954mas@mail.ru>
2019-10-06 11:24:18 +0300

    man phrases

--- scenes/game/game_scene.lua
+++ scenes/game/game_scene.lua
@@ -4,6 +4,15 @@ local GAME_CONTROLLER = require "scenes.game.model.game_controller"
 local SM = require "libs.sm.sm"
 local FaceView = require "common.face.face_view"
 
+local MAN_PHRASES = {
+    "Alince still my face.I need to make new from nothing.",
+    "I forgot my face at home.I need to make new from nothing.",
+    "My previous girl still my face.I need to make new from nothing.",
+    "FBI is looking for me.I need to make new from nothing.",
+    "My race will capture this planet.I need new face.",
+    "What happened yesterday and where is my face.I need new face."
+}
+
 ---@class GameScene:Scene
 local Scene = BaseScene:subclass("GameScene")
 function Scene:initialize()
@@ -15,6 +24,8 @@ function Scene:on_show()
     self.face_view = FaceView("/man",GAME_CONTROLLER.level.face)
     self.face_ideal = FaceView("/man_ideal",GAME_CONTROLLER.level.face_ideal)
     GAME_CONTROLLER.level.face:set_view(self.face_view)
+    label.set_text("/lbl_start#label",COMMON.LUME.randomchoice(MAN_PHRASES))
+    msg.post("/lbl_start#label",COMMON.HASHES.MSG_ENABLE)
 end
 
 function Scene:on_hide()
@@ -33,6 +44,10 @@ function Scene:on_update(dt)
 end
 
 function Scene:on_input(action_id, action)
+    if action_id == COMMON.HASHES.INPUT_TOUCH and action.pressed then
+        msg.post("/lbl_start#label",COMMON.HASHES.MSG_DISABLE)
+    end
+
     return GAME_CONTROLLER:on_input(action_id,action)
 end
 
